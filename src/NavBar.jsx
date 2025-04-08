5d4f1a1142ebd462921442188ca82ee6ff0cc8ef
Bao <baohtrang@gmail.com>
2025-04-07 21:59:10 -0700

    first commit

---
+++ src/NavBar.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './NavBar.css'
+
+function NavBar() {
+
+    return (
+        <>
+            <h1>Bao is cool</h1>
+            <h2>Cheese</h2>
+
+
+        </>
+    )
+}
+
+export default NavBar
