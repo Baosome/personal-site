89685288f8a126f8ce6978e117c12b9eb36e4c5e
Bao <baohtrang@gmail.com>
2025-04-07 22:33:19 -0700

    try out some fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import NavBar from './NavBar.jsx'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavBar></NavBar>
+      <BrowserRouter basename = "import.meta.env.BASE_URL">
+          <Routes>
+            <Route path = "*" element={<NavBar />}/>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
